320c463fd0b6f79b91f708ef865c69df8ff0d501
Fernando Peguero <fernandopeguerofs@gmail.com>
2024-09-21 21:13:57 -0400

    Add logic to change the images and text on the app

--- app/src/main/java/com/example/lemonade/MainActivity.kt
+++ app/src/main/java/com/example/lemonade/MainActivity.kt
@@ -13,6 +13,10 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
@@ -37,6 +41,23 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun LemonadeApp(modifier: Modifier = Modifier) {
 
+    var stepCounter by remember { mutableIntStateOf(0) }
+    val squeezeCounter = (2..4).random()
+    var counter = 0
+
+    val currentImage = when(stepCounter) {
+        0 -> painterResource(id = R.drawable.lemon_tree)
+        1 -> painterResource(id = R.drawable.lemon_squeeze)
+        2 -> painterResource(id = R.drawable.lemon_drink)
+        else -> painterResource(id = R.drawable.lemon_restart)
+    }
+
+    val currentText = when(stepCounter) {
+        0 -> stringResource(id = R.string.select_lemon)
+        1 -> stringResource(id = R.string.squeeze_lemon)
+        2 -> stringResource(id = R.string.drink_lemonade)
+        else -> stringResource(id = R.string.empty_glass)
+    }
 
 
     Column(
@@ -45,15 +66,28 @@ fun LemonadeApp(modifier: Modifier = Modifier) {
         modifier = modifier
     ) {
         Button(
-            onClick = { },
+            onClick = {
+                if(stepCounter == 3) {
+                    stepCounter = 0
+                } else {
+                    if(stepCounter == 1){
+                        if(counter == squeezeCounter){
+                            stepCounter++
+                        }
+                        counter++
+                    } else {
+                        stepCounter++
+                    }
+
+                } },
         ) {
-            Image(painter = painterResource(id = R.drawable.lemon_tree), contentDescription = stringResource(
+            Image(painter = currentImage, contentDescription = stringResource(
                 id = R.string.lemon_tree
             ))
         }
 
         Spacer(Modifier.padding(16.dp))
-        Text(text = stringResource(id = R.string.select_lemon))
+        Text(text = currentText)
     }
 }
 

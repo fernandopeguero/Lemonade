682031497167f9120c7ec74bb8ac49a1645b6db9
Fernando Peguero <fernandopeguerofs@gmail.com>
2024-09-21 21:58:14 -0400

    Add styling to component to make app more appealing

--- app/src/main/java/com/example/lemonade/MainActivity.kt
+++ app/src/main/java/com/example/lemonade/MainActivity.kt
@@ -4,14 +4,22 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults.topAppBarColors
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
@@ -19,19 +27,47 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.lemonade.ui.theme.LemonadeTheme
 
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             LemonadeTheme {
-               LemonadeApp(Modifier.fillMaxSize())
+                Scaffold(
+
+                    topBar =  {
+                        TopAppBar(
+                            colors = topAppBarColors(
+                                containerColor = MaterialTheme.colorScheme.primaryContainer,
+                                titleContentColor = MaterialTheme.colorScheme.primary,
+                            ),
+                            title = {
+                                Text(
+                                    text = "Lemonade",
+                                    textAlign = TextAlign.Center,
+
+                                    modifier = Modifier.fillMaxWidth()
+
+                                )
+                            }
+                        )
+                    }
+
+                ) { innerPadding ->
+                    LemonadeApp(Modifier.fillMaxSize().padding(innerPadding))
+                }
+
             }
         }
     }
@@ -80,6 +116,8 @@ fun LemonadeApp(modifier: Modifier = Modifier) {
                     }
 
                 } },
+            border = BorderStroke(2.dp , Color(105, 205, 216)),
+            shape = RoundedCornerShape(15)
         ) {
             Image(painter = currentImage, contentDescription = stringResource(
                 id = R.string.lemon_tree
@@ -87,7 +125,11 @@ fun LemonadeApp(modifier: Modifier = Modifier) {
         }
 
         Spacer(Modifier.padding(16.dp))
-        Text(text = currentText)
+        Text(
+            text = currentText,
+            fontSize = 18.sp,
+            fontWeight = FontWeight.Bold
+        )
     }
 }
 

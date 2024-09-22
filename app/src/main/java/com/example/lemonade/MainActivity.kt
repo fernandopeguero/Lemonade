8f40529372ac866e85b30adf3974869a9c1e5010
Fernando Peguero <fernandopeguerofs@gmail.com>
2024-09-21 20:58:27 -0400

    Add the visual design and component positioning

--- app/src/main/java/com/example/lemonade/MainActivity.kt
+++ app/src/main/java/com/example/lemonade/MainActivity.kt
@@ -4,10 +4,21 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.lemonade.ui.theme.LemonadeTheme
 
 class MainActivity : ComponentActivity() {
@@ -26,9 +37,27 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun LemonadeApp(modifier: Modifier = Modifier) {
 
+
+
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = modifier
+    ) {
+        Button(
+            onClick = { },
+        ) {
+            Image(painter = painterResource(id = R.drawable.lemon_tree), contentDescription = stringResource(
+                id = R.string.lemon_tree
+            ))
+        }
+
+        Spacer(Modifier.padding(16.dp))
+        Text(text = stringResource(id = R.string.select_lemon))
+    }
 }
 
-@Preview
+@Preview(showBackground = true)
 @Composable
 fun LemonadePreview() {
     LemonadeTheme {
